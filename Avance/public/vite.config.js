869de03365277bf156bbb800037e7f1f09cf616c
zmcedillo <cedillo182002@gmail.com>
2025-03-03 00:42:12 -0600

    modificacion de vite config y reincorporacion del redirect en render

--- Avance/public/vite.config.js
+++ Avance/public/vite.config.js
@@ -1,11 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Render espera los archivos en "dist"
+    outDir: 'dist',
   },
-  base: './',
-})
+  server: {
+    historyApiFallback: true, // Permite manejar rutas en desarrollo
+  },
+  base: '/', // Asegura que las rutas sean absolutas
+});
